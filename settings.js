589d2510b7f45506ad395f8bac8b55f2440c2556
AzzamSul <108897012+AzzamSul@users.noreply.github.com>
2022-07-08 02:23:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,22 +22,22 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6288214772063','6288214772063','6288214772063'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6288214772063'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6288214772063' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'MilkyStore' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Bot-Milky' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'MilkyStore' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.ig = 'https://instagram.com/storebymilky?igshid=YmMyMTA2M2Y='
+global.mygc = 'https://chat.whatsapp.com/CHbHU8JMRIlFQaGczdwlbH'
+global.myweb = 'myurls.co/storebymilky'
+global.email = 'azzamsul7@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
